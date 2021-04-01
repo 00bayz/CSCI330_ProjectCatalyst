8f3812e963812b2fc5ddd660268f8e26746075f2
00bayz <zhuoyu.yang@ndus.edu>
2021-04-01 17:51:46 -0500

    [Dev] Create buttons on landing (home) page. Buttons have no functionality at this point.

--- CSCI330_ProjectCatalyst/Form1.Designer.cs
+++ CSCI330_ProjectCatalyst/Form1.Designer.cs
@@ -29,7 +29,67 @@ namespace CSCI330_ProjectCatalyst
         /// </summary>
         private void InitializeComponent()
         {
+            this.TodoButton = new System.Windows.Forms.Button();
+            this.PomodoroButton = new System.Windows.Forms.Button();
+            this.CalendarButton = new System.Windows.Forms.Button();
+            this.TrackerButton = new System.Windows.Forms.Button();
+            this.RemindersButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // TodoButton
+            // 
+            this.TodoButton.Font = new System.Drawing.Font("Liberation Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TodoButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TodoButton.Location = new System.Drawing.Point(41, 32);
+            this.TodoButton.Name = "TodoButton";
+            this.TodoButton.Size = new System.Drawing.Size(160, 60);
+            this.TodoButton.TabIndex = 0;
+            this.TodoButton.Text = "To-Do List";
+            this.TodoButton.UseVisualStyleBackColor = true;
+            // 
+            // PomodoroButton
+            // 
+            this.PomodoroButton.Font = new System.Drawing.Font("Liberation Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PomodoroButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.PomodoroButton.Location = new System.Drawing.Point(41, 113);
+            this.PomodoroButton.Name = "PomodoroButton";
+            this.PomodoroButton.Size = new System.Drawing.Size(160, 60);
+            this.PomodoroButton.TabIndex = 1;
+            this.PomodoroButton.Text = "Pomodoro Timer";
+            this.PomodoroButton.UseVisualStyleBackColor = true;
+            // 
+            // CalendarButton
+            // 
+            this.CalendarButton.Font = new System.Drawing.Font("Liberation Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CalendarButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.CalendarButton.Location = new System.Drawing.Point(41, 202);
+            this.CalendarButton.Name = "CalendarButton";
+            this.CalendarButton.Size = new System.Drawing.Size(160, 60);
+            this.CalendarButton.TabIndex = 2;
+            this.CalendarButton.Text = "Calendar";
+            this.CalendarButton.UseVisualStyleBackColor = true;
+            // 
+            // TrackerButton
+            // 
+            this.TrackerButton.Font = new System.Drawing.Font("Liberation Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TrackerButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.TrackerButton.Location = new System.Drawing.Point(41, 291);
+            this.TrackerButton.Name = "TrackerButton";
+            this.TrackerButton.Size = new System.Drawing.Size(160, 60);
+            this.TrackerButton.TabIndex = 3;
+            this.TrackerButton.Text = "Habit Tracker";
+            this.TrackerButton.UseVisualStyleBackColor = true;
+            // 
+            // RemindersButton
+            // 
+            this.RemindersButton.Font = new System.Drawing.Font("Liberation Mono", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RemindersButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.RemindersButton.Location = new System.Drawing.Point(41, 384);
+            this.RemindersButton.Name = "RemindersButton";
+            this.RemindersButton.Size = new System.Drawing.Size(160, 60);
+            this.RemindersButton.TabIndex = 4;
+            this.RemindersButton.Text = "Reminders";
+            this.RemindersButton.UseVisualStyleBackColor = true;
             // 
             // CatalystMain
             // 
@@ -37,6 +97,11 @@ namespace CSCI330_ProjectCatalyst
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(1067, 519);
+            this.Controls.Add(this.RemindersButton);
+            this.Controls.Add(this.TrackerButton);
+            this.Controls.Add(this.CalendarButton);
+            this.Controls.Add(this.PomodoroButton);
+            this.Controls.Add(this.TodoButton);
             this.Font = new System.Drawing.Font("Liberation Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
@@ -47,6 +112,12 @@ namespace CSCI330_ProjectCatalyst
         }
 
         #endregion
+
+        private System.Windows.Forms.Button TodoButton;
+        private System.Windows.Forms.Button PomodoroButton;
+        private System.Windows.Forms.Button CalendarButton;
+        private System.Windows.Forms.Button TrackerButton;
+        private System.Windows.Forms.Button RemindersButton;
     }
 }
 
