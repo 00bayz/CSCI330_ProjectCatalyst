503f5c4e8c9d8e0df8a6d33d4b4b3e58938456ef
00bayz <zhuoyu.yang@ndus.edu>
2021-04-01 17:18:12 -0500

    [Dev] Set up font, fontsize, font color, and background color.

--- CSCI330_ProjectCatalyst/Form1.Designer.cs
+++ CSCI330_ProjectCatalyst/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CSCI330_ProjectCatalyst
 {
-    partial class Form1
+    partial class CatalystMain
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,21 @@ namespace CSCI330_ProjectCatalyst
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CatalystMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1067, 519);
+            this.Font = new System.Drawing.Font("Liberation Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "CatalystMain";
+            this.Text = "Catalyst | Home";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
