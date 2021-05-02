f017968a6267340ac610d6ae6b29a4c77f813c7f
CDAGaming <cstack2011@yahoo.com>
2021-05-02 16:44:07 -0500

    [Change] Module Data

--- CSCI330_ProjectCatalyst/Calendar.Designer.cs
+++ CSCI330_ProjectCatalyst/Calendar.Designer.cs
@@ -36,21 +36,35 @@ namespace CSCI330_ProjectCatalyst
             this.label1 = new System.Windows.Forms.Label();
             this.CloseButton = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.SaveAllButton = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SchedulerList = new System.Windows.Forms.ListBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.ToText = new System.Windows.Forms.TextBox();
+            this.FromText = new System.Windows.Forms.TextBox();
+            this.DetailsText = new System.Windows.Forms.TextBox();
+            this.FromLabel = new System.Windows.Forms.Label();
+            this.ToLabel = new System.Windows.Forms.Label();
+            this.DetailsLabel = new System.Windows.Forms.Label();
+            this.SyncButton = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // EntryText
             // 
-            this.EntryText.Location = new System.Drawing.Point(40, 46);
+            this.EntryText.Location = new System.Drawing.Point(34, 46);
             this.EntryText.Name = "EntryText";
             this.EntryText.Size = new System.Drawing.Size(261, 29);
             this.EntryText.TabIndex = 0;
             // 
             // SearchButton
             // 
-            this.SearchButton.Location = new System.Drawing.Point(307, 45);
+            this.SearchButton.Location = new System.Drawing.Point(301, 45);
             this.SearchButton.Name = "SearchButton";
             this.SearchButton.Size = new System.Drawing.Size(90, 30);
             this.SearchButton.TabIndex = 1;
@@ -67,7 +81,7 @@ namespace CSCI330_ProjectCatalyst
             this.groupBox1.Size = new System.Drawing.Size(243, 200);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Calender";
+            this.groupBox1.Text = "Calendar";
             // 
             // CalenderDisplay
             // 
@@ -87,7 +101,7 @@ namespace CSCI330_ProjectCatalyst
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(178, 21);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Calender and Scheduler:";
+            this.label1.Text = "Calendar and Scheduler:";
             // 
             // CloseButton
             // 
@@ -98,9 +112,14 @@ namespace CSCI330_ProjectCatalyst
             this.CloseButton.TabIndex = 4;
             this.CloseButton.Text = "Close";
             this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
             // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.SyncButton);
+            this.groupBox2.Controls.Add(this.RemoveButton);
+            this.groupBox2.Controls.Add(this.AddButton);
+            this.groupBox2.Controls.Add(this.SaveAllButton);
             this.groupBox2.Controls.Add(this.CloseButton);
             this.groupBox2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.groupBox2.Location = new System.Drawing.Point(12, 328);
@@ -110,15 +129,140 @@ namespace CSCI330_ProjectCatalyst
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Scheduler Controls";
             // 
+            // RemoveButton
+            // 
+            this.RemoveButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.RemoveButton.Location = new System.Drawing.Point(5, 74);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(90, 30);
+            this.RemoveButton.TabIndex = 7;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.AddButton.Location = new System.Drawing.Point(6, 28);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(90, 30);
+            this.AddButton.TabIndex = 6;
+            this.AddButton.Text = "Add New";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // SaveAllButton
+            // 
+            this.SaveAllButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SaveAllButton.Location = new System.Drawing.Point(289, 28);
+            this.SaveAllButton.Name = "SaveAllButton";
+            this.SaveAllButton.Size = new System.Drawing.Size(90, 30);
+            this.SaveAllButton.TabIndex = 5;
+            this.SaveAllButton.Text = "Save All";
+            this.SaveAllButton.UseVisualStyleBackColor = true;
+            this.SaveAllButton.Click += new System.EventHandler(this.SaveAllButton_Click);
+            // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.SchedulerList);
             this.groupBox3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.groupBox3.Location = new System.Drawing.Point(403, 12);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(385, 426);
+            this.groupBox3.Size = new System.Drawing.Size(385, 293);
             this.groupBox3.TabIndex = 6;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Scheduler";
+            // 
+            // SchedulerList
+            // 
+            this.SchedulerList.FormattingEnabled = true;
+            this.SchedulerList.ItemHeight = 21;
+            this.SchedulerList.Location = new System.Drawing.Point(7, 29);
+            this.SchedulerList.Name = "SchedulerList";
+            this.SchedulerList.Size = new System.Drawing.Size(372, 256);
+            this.SchedulerList.TabIndex = 0;
+            this.SchedulerList.SelectedValueChanged += new System.EventHandler(this.SchedulerList_SelectedValueChanged);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.ToText);
+            this.groupBox4.Controls.Add(this.FromText);
+            this.groupBox4.Controls.Add(this.DetailsText);
+            this.groupBox4.Controls.Add(this.FromLabel);
+            this.groupBox4.Controls.Add(this.ToLabel);
+            this.groupBox4.Controls.Add(this.DetailsLabel);
+            this.groupBox4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.groupBox4.Location = new System.Drawing.Point(404, 311);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(383, 126);
+            this.groupBox4.TabIndex = 7;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Event Details";
+            // 
+            // ToText
+            // 
+            this.ToText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ToText.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ToText.Location = new System.Drawing.Point(75, 91);
+            this.ToText.Name = "ToText";
+            this.ToText.Size = new System.Drawing.Size(302, 29);
+            this.ToText.TabIndex = 5;
+            // 
+            // FromText
+            // 
+            this.FromText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.FromText.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FromText.Location = new System.Drawing.Point(75, 55);
+            this.FromText.Name = "FromText";
+            this.FromText.Size = new System.Drawing.Size(302, 29);
+            this.FromText.TabIndex = 4;
+            // 
+            // DetailsText
+            // 
+            this.DetailsText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.DetailsText.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DetailsText.Location = new System.Drawing.Point(75, 21);
+            this.DetailsText.Name = "DetailsText";
+            this.DetailsText.Size = new System.Drawing.Size(302, 29);
+            this.DetailsText.TabIndex = 3;
+            // 
+            // FromLabel
+            // 
+            this.FromLabel.AutoSize = true;
+            this.FromLabel.Location = new System.Drawing.Point(9, 58);
+            this.FromLabel.Name = "FromLabel";
+            this.FromLabel.Size = new System.Drawing.Size(50, 21);
+            this.FromLabel.TabIndex = 2;
+            this.FromLabel.Text = "From:";
+            // 
+            // ToLabel
+            // 
+            this.ToLabel.AutoSize = true;
+            this.ToLabel.Location = new System.Drawing.Point(13, 94);
+            this.ToLabel.Name = "ToLabel";
+            this.ToLabel.Size = new System.Drawing.Size(46, 21);
+            this.ToLabel.TabIndex = 1;
+            this.ToLabel.Text = "Until:";
+            // 
+            // DetailsLabel
+            // 
+            this.DetailsLabel.AutoSize = true;
+            this.DetailsLabel.Location = new System.Drawing.Point(9, 24);
+            this.DetailsLabel.Name = "DetailsLabel";
+            this.DetailsLabel.Size = new System.Drawing.Size(60, 21);
+            this.DetailsLabel.TabIndex = 0;
+            this.DetailsLabel.Text = "Details:";
+            // 
+            // SyncButton
+            // 
+            this.SyncButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.SyncButton.Location = new System.Drawing.Point(102, 28);
+            this.SyncButton.Name = "SyncButton";
+            this.SyncButton.Size = new System.Drawing.Size(90, 30);
+            this.SyncButton.TabIndex = 8;
+            this.SyncButton.Text = "Sync";
+            this.SyncButton.UseVisualStyleBackColor = true;
+            this.SyncButton.Click += new System.EventHandler(this.SyncSelectedInfo);
             // 
             // Calendar
             // 
@@ -126,6 +270,7 @@ namespace CSCI330_ProjectCatalyst
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label1);
@@ -142,6 +287,9 @@ namespace CSCI330_ProjectCatalyst
             this.Load += new System.EventHandler(this.Calendar_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -157,5 +305,17 @@ namespace CSCI330_ProjectCatalyst
         private System.Windows.Forms.Button CloseButton;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox SchedulerList;
+        private System.Windows.Forms.Button SaveAllButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox ToText;
+        private System.Windows.Forms.TextBox FromText;
+        private System.Windows.Forms.TextBox DetailsText;
+        private System.Windows.Forms.Label FromLabel;
+        private System.Windows.Forms.Label ToLabel;
+        private System.Windows.Forms.Label DetailsLabel;
+        private System.Windows.Forms.Button SyncButton;
     }
 }
--- CSCI330_ProjectCatalyst/Calendar.cs
+++ CSCI330_ProjectCatalyst/Calendar.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CSCI330_ProjectCatalyst.Utilities;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +15,19 @@ namespace CSCI330_ProjectCatalyst
 {
     public partial class Calendar : Form
     {
+        public class CalendarInfo
+        {
+            public string details { get; set; }
+            public string from { get; set; }
+            public string to { get; set; }
+        }
+
+        string defaultLocation = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "calendar" + Path.DirectorySeparatorChar + "data.json";
+        Dictionary<string, List<CalendarInfo>> cachedJsonInfo;
+        List<CalendarInfo> calenderData = new List<CalendarInfo>();
+        CalendarInfo selectedInfo;
+        bool listeningForChange = false;
+
         public Calendar()
         {
             InitializeComponent();
@@ -19,17 +35,134 @@ namespace CSCI330_ProjectCatalyst
 
         private void CalenderDisplay_DateChanged(object sender, DateRangeEventArgs e)
         {
-            EntryText.Text = CalenderDisplay.SelectionStart.ToString();
+            EntryText.Text = CalenderDisplay.SelectionStart.ToString("MM/dd/yyyy");
+            LoadScheduleData(defaultLocation, EntryText.Text);
         }
 
         private void Calendar_Load(object sender, EventArgs e)
         {
-            EntryText.Text = CalenderDisplay.SelectionStart.ToString();
+            EntryText.Text = CalenderDisplay.SelectionStart.ToString("MM/dd/yyyy");
+            LoadScheduleData(defaultLocation, EntryText.Text);
         }
 
         private void SearchButton_Click(object sender, EventArgs e)
         {
             CalenderDisplay.SelectionStart = DateTime.Parse(EntryText.Text);
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        public void LoadScheduleData(string location, string date)
+        {
+            calenderData.Clear();
+
+            cachedJsonInfo = JsonConvert.DeserializeObject<Dictionary<string, List<CalendarInfo>>>(File.ReadAllText(location));
+            if (cachedJsonInfo.ContainsKey(date))
+            {
+                cachedJsonInfo.TryGetValue(date, out calenderData);
+                Console.WriteLine("Found Data for {0}: {1} values", date, calenderData.Count);
+            }
+            RefreshListView();
+        }
+
+        public void SaveSchedulerData(string location, string date, List<CalendarInfo> info)
+        {
+            Console.WriteLine("Writing for date {0}: {1} total values", date, info.Count);
+            cachedJsonInfo[date] = info;
+            using (StreamWriter file = File.CreateText(location))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, cachedJsonInfo);
+            }
+            Console.WriteLine("Saved Successfully to {0}", location);
+        }
+
+        public void RefreshListView()
+        {
+            SchedulerList.Items.Clear();
+            foreach (CalendarInfo info in calenderData)
+            {
+                SchedulerList.Items.Add(info.details);
+            }
+            ToggleButtons(false);
+        }
+
+        public void ToggleButtons(bool hasSelectedItem)
+        {
+            //AddButton.Enabled = hasSelectedItem;
+            RemoveButton.Enabled = hasSelectedItem;
+            DetailsText.Enabled = hasSelectedItem;
+            ToText.Enabled = hasSelectedItem;
+            FromText.Enabled = hasSelectedItem;
+        }
+
+        private void SaveAllButton_Click(object sender, EventArgs e)
+        {
+            SaveSchedulerData(defaultLocation, EntryText.Text, calenderData);
+        }
+
+        private void SchedulerList_SelectedValueChanged(object sender, EventArgs e)
+        {
+            selectedInfo = FindInfoFromField(SchedulerList.SelectedItem.ToString());
+            bool hasData = selectedInfo != null;
+            if (hasData)
+            {
+                DetailsText.Text = selectedInfo.details;
+                FromText.Text = selectedInfo.from;
+                ToText.Text = selectedInfo.to;
+            }
+            ToggleButtons(hasData);
+        }
+
+        public CalendarInfo FindInfoFromField(string details)
+        {
+            foreach (CalendarInfo value in calenderData)
+            {
+                if (value.details == details)
+                {
+                    return value;
+                }
+            }
+            return null;
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            CalendarInfo infoToRemove = FindInfoFromField(SchedulerList.SelectedItem.ToString());
+            if (infoToRemove != null)
+            {
+                calenderData.Remove(infoToRemove);
+            }
+            RefreshListView();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            RandomGenerator generator = new RandomGenerator();
+            calenderData.Add(new CalendarInfo()
+            {
+                details = string.Format("New Event {0}", generator.RandomString(5)),
+                from = "12:00 AM",
+                to = "12:00 PM"
+            });
+            RefreshListView();
+        }
+
+        private void SyncSelectedInfo(object sender, EventArgs e)
+        {
+            if (selectedInfo != null)
+            {
+                calenderData.Remove(selectedInfo);
+                selectedInfo.details = DetailsText.Text;
+                selectedInfo.from = FromText.Text;
+                selectedInfo.to = ToText.Text;
+                calenderData.Add(selectedInfo);
+
+                RefreshListView();
+            }
         }
     }
 }
